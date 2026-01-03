fc458459448f5f7b3897f72cb0a8dc6ae7b235ec
freitaseric <contato@freitaseric.com>
2026-01-02 21:47:15 -0400

    feat(web): setup netlify deploy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,12 +4,12 @@ import {tanstackStart} from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import {nitro} from 'nitro/vite'
+import netlify from "@netlify/vite-plugin";
 
 const config = defineConfig({
     plugins: [
         devtools(),
-        nitro(),
+        netlify(),
         viteTsConfigPaths({
             projects: ['./tsconfig.json'],
         }),
